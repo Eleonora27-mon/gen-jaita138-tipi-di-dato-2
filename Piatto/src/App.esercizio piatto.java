f95c9428386d6ffc0752e8180ccc214bf56db7e8
Eleonora27-mon <monterisieleonora@gmail.com>
2024-12-03 15:16:18 +0100

    piatto

---
+++ Piatto/src/App.esercizio piatto.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Crea il tuo piatto da ristorante");
+
+        System.out.println("Nome del piatto");
+        String nome del piatto = sc.nextLine();
+
+        System.out.println("Descrizione del piatto");
+        String descrizione del piatto = sc.nextLine()
+
+        System.out.println("Prezzo del piatto");
+        double  prezzo del piatto = sc.nextDouble ();
+
+        System.out.println("Disponibilità del piatto");
+        int disponibilità del piatto = sc.nextInt ();
+        sc.nextLine(): //consumo carattere di fine riga
+
+        System.out.println("Tipologia del piatto");
+        byte tipologia di piatto = sc.nextByte();
+
+        System.out.println("Presenza di allergeni");
+        String presenza di allergeni = sc.nextLine();
+
+        System.out.println("Foto del piatto");
+        String foto del piatto = sc.nextLine();
+
+        System.out.println("Variazione del piatto");
+        String variazione del piatto = sc.nextLine();
+
+        System.out.println("Calorie del piatto");
+        short calorie del piatto = sc.nextShort();
+
+        sc.close();
+
+    // ----------------------------------------------------------------
+
+
+        System.out.println("Il tuo piatto da ristorante:");
+        
+
+        System.out.println("Nome" + nome);
+        System.out.println("Descizione del piatto" + descrizione);
+        System.out.println("Prezzo del piatto" + prezzo);
+        System.out.println("Disponibilità del piatto" + disponibilità del piatto);
+        System.out.println("Tipologia del piatto" + tipologia del piatto);
+        System.out.println("Presenza di allergeni" + presenza di allergeni);
+        System.out.println("Foto del piatto" + foto del piatto);
+        System.out.println("Variazione del piatto" + variazione del piatto);
+        System.out.println("Calorie del piatto" + calorie del piatto);
+
+
+
+
+
+        )
+    } 
+
+    
